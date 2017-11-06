b0c0f378fb27bc267de51d3cfcf43463d4fb8881
Estrés <desarrollo@estresarte.com>
2017-11-06 18:12:07 +0100

    Cambio petición
    Cambio texto de la petición

--- includes/peticion.php
+++ includes/peticion.php
@@ -18,16 +18,15 @@
 
           <br/><br/>• Adoptar e instar al gobierno a que adopte medidas para aplicar las recomendaciones de los órganos internacionales en relación con la Ley de Amnistía y con la imprescriptibilidad de los crímenes de derecho internacional, incluidas las del Comité de Derechos Humanos, el Consejo de Europa, el Grupo de Trabajo sobre Desapariciones Forzadas o Involuntarias, y el Comité contra la Tortura.
 
-          <br/><br/>• Impulsar medidas legislativas para garantizar el derecho a la verdad de las víctimas y sus familiares, entre otras, la conservación de archivos, exhumación de fosas, identificación y devolución de restos a familiares, o la promoción de la creación de una Comisión de la Verdad con el mandato de investigar los abusos graves, según el derecho internacional, cometidos durante la Guerra Civil y el franquismo.
+          <br/><br/>• Impulsar medidas legislativas para garantizar el derecho a la verdad de las víctimas y sus familiares, entre otras, la conservación de archivos, la modificación de la Ley de Secretos Oficiales, la exhumación de fosas, identificación y devolución de restos a sus familiares, o la promoción de la creación de una Comisión de la Verdad con el mandato de investigar los abusos graves, según el derecho internacional, cometidos durante la Guerra Civil y el franquismo.
 
-          <br/><br/>• Adoptar medidas que contribuyan a la búsqueda e identificación de los niños y niñas que podrían haber sido víctimas de apropiación, desaparición forzada y/o sustitución de su identidad.
+          <br/><br/>• Promover iniciativas legislativas destinadas a la búsqueda e identificación de los niños y niñas que podrían haber sido víctimas de apropiación, desaparición forzada y/o sustitución de su identidad.
 
           <br/><br/>• Ampliar el reconocimiento y la cobertura de los programas de reparación para incluir todas las categorías de víctimas que hayan quedado fuera de los programas existentes. En este mismo sentido, ampliar la cobertura del Estatuto de la Víctima a las víctimas del franquismo y la Guerra Civil.
 
           <br/><br/>• Impulsar medidas legislativas para hacer efectiva la nulidad de las sentencias adoptadas durante la Guerra Civil y el franquismo en violación de los principios fundamentales del derecho a un juicio justo.
 
-          <br/><br/>• Formular disculpas públicas que precisen la naturaleza de los actos criminales, sus circunstancias y la realidad concreta de los daños infligidos.
-
+          <br/><br/>• Impulsar medidas que contemplen la formulación de disculpas públicas que trasciendan un mero reconocimiento genérico y que precisen la naturaleza de los actos criminales, sus circunstancias y la realidad concreta de los daños infligidos. En este sentido, establecer un Día Institucional en recuerdo y homenaje a las víctimas de la Guerra Civil y el franquismo.
 
           <br/><br/>Atentamente,
         </p>
