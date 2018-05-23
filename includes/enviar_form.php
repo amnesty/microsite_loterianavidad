e409293f6a848cc184d37b201d07f33dbb09114a
frodriguez <frodriguez@es.amnesty.org>
2018-05-23 11:53:28 +0200

    Cambios para envio API

--- includes/enviar_form.php
+++ includes/enviar_form.php
@@ -161,10 +161,10 @@ if( $_POST['guardar_form'] ) {
 
 					// si no existe el member, lo creamos internamente
 					if(!isset($member_id)) {
-						$member = post_member_ai($email, $nombre, $apellidos, $telefono, $pais_siglas, $pais_nombre);
+						$member = post_member_ai($email, $nombre, $apellidos, $telefono, $pais_siglas, $pais_nombre, $estado, $no_fundraising);
 						$member_id = $member['id'];
 						//insertamos el member en la plaforma de envio de correos
-						post_member_experian($member_id, $nombre, $apellidos, $email, $telefono, $pais_siglas, $pais_nombre);
+						post_member_experian($member_id, $nombre, $apellidos, $email, $telefono, $pais_siglas, $pais_nombre, $estado, $no_fundraising);
 					}
 					// vemos si existe la purchase internamente
 					$purchase = get_purchase_by_member_product($product_id, $member_id);
