3a912076d179d7b3199daba55902c7ad01025cf7
jllcarrasco <jllcarrasco@gmail.com>
2018-12-12 11:20:06 +0100

    incluir tag manager

--- antonio-narvaez.php
+++ antonio-narvaez.php
@@ -60,6 +60,15 @@ $caso = 1;
     <!-- cli-style.css -->
     <link href="css/cli-style.css" rel="stylesheet">
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5JHK4F4');</script>
+<!-- End Google Tag Manager -->
+
+
     <!-- jQuery 1.12.4 -->
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- tether.min Obligatorio para BS tooltip -->
@@ -79,6 +88,11 @@ $caso = 1;
 
 </head>
 <body class="template-caso">
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JHK4F4"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
     <!-- Modal Carta petición -->
     <?php include "includes/peticion.php";?>
--- seguimos-pidiendo-justicia.php
+++ seguimos-pidiendo-justicia.php
@@ -60,6 +60,14 @@ $caso = 2;
     <!-- cli-style.css -->
     <link href="css/cli-style.css" rel="stylesheet">
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5JHK4F4');</script>
+<!-- End Google Tag Manager -->
+
     <!-- jQuery 1.12.4 -->
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- tether.min Obligatorio para BS tooltip -->
@@ -80,6 +88,10 @@ $caso = 2;
 </head>
 <body class="template-caso">
 
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JHK4F4"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
     <?php include "includes/header-seguimos.php";?>
 
