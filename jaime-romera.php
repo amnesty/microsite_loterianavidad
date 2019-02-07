d76639442336e3fd906802989f992817a4b51162
jllcarrasco <jllcarrasco@gmail.com>
2019-02-07 15:31:30 +0100

    cambio en tag manager

--- jaime-romera.php
+++ jaime-romera.php
@@ -86,6 +86,17 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     <!-- Tipografía Maven -->
     <link href="https://fonts.googleapis.com/css?family=Maven+Pro:400,700" rel="stylesheet">
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129198217-1"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-129198217-1', { 'optimize_id': 'GTM-WM7GM4Z'});
+</script>
+
+
 </head>
 <body class="template-caso">
 
