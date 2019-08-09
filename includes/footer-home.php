9cdcd582e5b3e3132a41a63c42918d2936166fa3
jllcarrasco <jllcarrasco@gmail.com>
2019-08-09 16:09:33 +0200

    cambio en el pie de las paginas

---
+++ includes/footer-home.php
@@ -0,0 +1,27 @@
+<footer class="clearfix">
+  <div class="container">
+    <p class="title">Amnistía Internacional <?php echo date("Y"); ?></p>
+    <p>*Coste 1,2 euros (donación integra para la ONG). Servicio de SMS para recaudación de fondos en campaña de tipo solidario operado por Altiria TIC, <a href="http://www.altiria.com" target="_blank">www.altiria.com</a>, y la Asociación Española de Fundraising, <a href="http://www.aefundraising.org" target="_blank">www.aefundraising.org</a>, nº. atn. clte. 902 00 28 98, apdo. correos 36059 –28080 Madrid.<br/> Colaboran Movistar, Vodafone, Orange, Yoigo, Euskaltel y Telecable.<br/> Más información sobre el uso de los datos en <a href="https://www.es.amnesty.org/politica-de-privacidad" target="_blank">https://www.es.amnesty.org/politica-de-privacidad</a></p>
+  </div>
+</footer>
+
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+  _paq.push(["setCookieDomain", "*.es.amnesty.org"]);
+  _paq.push(["setDomains", ["*.es.amnesty.org",]]);
+  _paq.push(['trackPageView']);
+  _paq.push(['trackVisibleContentImpressions']);
+  _paq.push(['enableLinkTracking']);
+  _paq.push(['MediaAnalytics::removePlayer', 'youtube']);
+  (function() {
+    var u="//estadisticas.es.amnesty.org/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//estadisticas.es.amnesty.org/piwik/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
