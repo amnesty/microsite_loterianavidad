a465ff01eb192b5ad29c31cc278c8a735f5a6d3a
jllcarrasco <jllcarrasco@gmail.com>
2021-10-08 13:19:47 +0200

    Update firma-form.php

--- includes/firma-form.php
+++ includes/firma-form.php
@@ -37,7 +37,7 @@ if ( isset($_GET['utm_campaign']) || isset($_GET['pk_kwd']) ){
 <?php
 $num_firmas_total = NUM_FIRMAS_TOTAL; // config.php -> Se puede modificar bajo demanda. Establecido a 5000
 $num_firmas = ""; // Guardar en esta variable "$num_firmas" el nº de firmas actualziado que se lleva para esta campaña.
-$num_firmas_bd_bak = "160190"; // Numero de firmas de la tabla datos_firmas_backup (83252) + datos_firmas (71331). Pte incorporar a tabla firmas. + firmas recogidas en Facebook (2570+474+1830+733)
+$num_firmas_bd_bak = "144540"; // Numero de firmas de la tabla datos_firmas_backup (83252) + datos_firmas (71331). Pte incorporar a tabla firmas. + firmas recogidas en Facebook (2570+474+1830+733) Ajustado a la fecha de entrega de firmas para que sea coherente.
 
 if($tabla == 'test_firmas')
   $num_firmas_bd_bak = 0;
