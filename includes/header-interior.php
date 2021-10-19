525387c3b3e981f0bd346b1776ccefdcd5727ef8
jllcarrasco <jllcarrasco@gmail.com>
2021-10-19 10:56:07 +0200

    Cierre del formulario

--- includes/header-interior.php
+++ includes/header-interior.php
@@ -2,7 +2,11 @@
     <div class="logo"><a data-e_c="justiciapornavidad" data-e_a="<?=$casos[$caso][2]?> logo header volver_campaña" data-e_l="<?=$casos[$caso][2]?> logo header volver_campaña" href="<?php echo URL_SITE; ?>?video=novideo" title="Amnistía Internacional" class="send-piwik-event"><img src="images/logo-amnistia-blanco.png" alt="Amnistía Internacional"></a></div>
     <div class="botones">
         <ul class="clearfix">
-            <li><a data-e_c="justiciapornavidad" data-e_a="<?=$casos[$caso][2]?> boton header firma" data-e_l="<?=$casos[$caso][2]?> boton header firma" href="#firma" title="Firma" class="btn-header animated-button btn-animate-cream btn-firma send-piwik-event">Firma</a></li>
+
+<!–– quitamos el botón en el cierre
+<li><a data-e_c="justiciapornavidad" data-e_a="<?=$casos[$caso][2]?> boton header firma" data-e_l="<?=$casos[$caso][2]?> boton header firma" href="#firma" title="Firma" class="btn-header animated-button btn-animate-cream btn-firma send-piwik-event">Firma</a></li>
+––>
+            
 		    <li><a data-e_c="justiciapornavidad" data-e_a="<?=$casos[$caso][2]?> boton header socio" data-e_l="<?=$casos[$caso][2]?> boton header socio" href="https://crm.es.amnesty.org/unete-a-amnistia/loteria/?origen=justiciapornavidad&caso=<?=$casos[$caso][2]?>" title="Hazte Socio/a" class="btn-header animated-button btn-animate-white btn-hazte-socio send-piwik-event" target="_blank">Hazte Socio/a</a></li>
         </ul>
     </div>
